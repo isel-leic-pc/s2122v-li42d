1d32ab6843cfbbc4aec53a963982790d905d9500
jmartins3 <jmartinsisel.pt>
2022-04-07 19:22:24 +0100

    support for cancellation and timeout

--- aula-04-05/src/main/kotlin/isel/leic/pc/monitors2/MessageBoard.kt
+++ aula-04-05/src/main/kotlin/isel/leic/pc/monitors2/MessageBoard.kt
@@ -1,7 +1,9 @@
 package isel.leic.pc.monitors2
 
+import isel.leic.pc.utils.await
 import isel.leic.pc.utils.dueTime
 import isel.leic.pc.utils.isPast
+import isel.leic.pc.utils.isZero
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 import kotlin.time.Duration
@@ -12,6 +14,7 @@ class MessageBoard<M> {
     private val monitor = ReentrantLock()
     private val msgPublished = monitor.newCondition()
 
+
     class PubMessage<M>(val msg : M?, exposureDuration: Duration) {
         val dueTime = exposureDuration.dueTime()
 
@@ -19,10 +22,11 @@ class MessageBoard<M> {
             get() { return !dueTime.isPast }
     }
 
-    var pub = PubMessage(null, 0.toDuration(DurationUnit.NANOSECONDS))
+    var pub =  PubMessage<M>(null, 0.toDuration(DurationUnit.MILLISECONDS))
+
     fun publish(message: M, exposureDuration: Duration) {
         monitor.withLock {
-
+            TODO()
         }
     }
 
@@ -31,9 +35,7 @@ class MessageBoard<M> {
         monitor.withLock {
              // fast path
             if (pub.isValid) return pub.msg
-
-            // to complete in next lecture
-            return null
+            TODO()
         }
     }
 
